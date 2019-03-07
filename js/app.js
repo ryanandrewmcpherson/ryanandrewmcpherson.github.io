06413e8d3b5be9b4033f258ed746aa01f8d7f066
ryanandrewmcpherson <ryanmcphersondev@gmail.com>
2019-03-06 20:45:15 -0500

    committing modal

--- js/app.js
+++ js/app.js
@@ -6,3 +6,26 @@ function myFunction() {
       x.style.display = "block";
     }
   }
+
+var modal = document.getElementById('myModal');
+
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "flex";
+  modal.style.justifyContent = "center";
+  modal.style.alignItems = "center";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
