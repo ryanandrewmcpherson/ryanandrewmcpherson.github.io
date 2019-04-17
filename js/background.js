274efe5d198e9ef6156b4af1c4eeaf7165c396ae
ryanandrewmcpherson <ryanmcphersondev@gmail.com>
2019-04-16 23:35:21 -0400

    awesome

--- js/background.js
+++ js/background.js
@@ -6,24 +6,37 @@ background.width = parseInt(window.getComputedStyle(document.querySelector("body
 document.querySelector("body").appendChild(background);
 
 
-var numCircles = 100;
-var interval = 1000/75;
-var xA = [];
-var yA = [];
-var vXA = [];
-var vYA = [];
-var sizeA = [];
-var colorA = [];
+let numCircles = 3;
+let interval = 1000/30;
+let xA;
+let yA;
+let vXA;
+let vYA;
+let sizeA;
+let colorA;
+
 
 function definecircles(){
+
+    xA = [];
+    yA = [];
+    vXA = [];
+    vYA = [];
+    sizeA = [];
+    colorA = [];
+
     for(let i = 0;i<numCircles;i++){
 
         let size = Math.floor(Math.random()*151) + 50;
         sizeA.push(size);
-
-        let x = Math.floor(Math.random()*(background.width + 1 - 2*size)) + size;
+        
+        
+        let x;
+        x = Math.floor(Math.random()*(background.width + 1 - 2*size)) + size;
         xA.push(x);
-        let y = Math.floor(Math.random()*(background.height + 1 - 2*size)) + size;
+        console.log(xA.indexOf(x));
+        let y;
+        y = Math.floor(Math.random()*(background.height + 1 - 2*size)) + size;
         yA.push(y);
 
         
@@ -47,48 +60,60 @@ function definecircles(){
             vY = Math.floor(Math.random()*81) - 40; 
         }
         vYA.push(vX);
-    }
-}
 
+    }
+};
 definecircles();
 
 window.setInterval(function(){
 
 ctx.clearRect(0,0,background.width,background.height);
 
-for(let j = 0;j<sizeA.length;j++){
-    if(xA[j] >= background.width  || xA[j]  <= 0){
-        vXA[j] = vXA[j]*-1;
-    }
-    if(yA[j] >= background.height || yA[j]  <= 0){
-        vYA[j] = vYA[j]*-1;
-    }
-
-    if(Math.abs(xA[j]-mouseclickX) <= sizeA[j] && Math.abs(yA[j]-mouseclickY) <= sizeA[j]){
-        sizeA.splice(j,1);
-        xA.splice(j,1);
-        yA.splice(j,1);
-        colorA.splice(j,1);
-        vYA.splice(j,1);
-        vXA.splice(j,1);
-    }
-
-    xA[j] = xA[j] + vXA[j]*interval/1000;
-    yA[j] = yA[j] + vYA[j]*interval/1000;
+for(let i = 0;i<sizeA.length;i++){
 
     
-    if(sizeA.length>0){
+
+    if(xA[i] >= background.width || xA[i]  <= 0){
+        vXA[i] = vXA[i]*-1;
+    }
+    if(yA[i] >= background.height || yA[i]  <= 0){
+        vYA[i] = vYA[i]*-1;
+    }
+    
+    if(mouseclickX != null){
+        if(Math.abs(mouseclickX - xA[i]) <= sizeA[i] && Math.abs(mouseclickY - yA[i]) <= sizeA[i]){
+            xA.splice(i,1);
+            yA.splice(i,1);
+            vXA.splice(i,1);
+            vYA.splice(i,1);
+            sizeA.splice(i,1);
+            colorA.splice(i,1);
+            
+        }
+
+    }
+
+
+    xA[i] = xA[i] + vXA[i]*interval/1000;
+    yA[i] = yA[i] + vYA[i]*interval/1000;
+
+    
+    if(sizeA.length != 0){  
         ctx.beginPath();
-        ctx.arc(xA[j],yA[j],sizeA[j],0 , 2*Math.PI)
-        strokeColor = colorA[j];
+        ctx.arc(xA[i],yA[i],sizeA[i],0 , 2*Math.PI)
+        strokeColor = colorA[i];
         ctx.fillStyle = strokeColor;
         ctx.fill();
     }
     else{
+        mouseclickX = null;
+        mouseclickY = null;
         definecircles();
-    }
+        break;
+    }  
 }
-
+    mouseclickX = null;
+    mouseclickY = null;
 },interval);
 
 
@@ -99,8 +124,9 @@ window.addEventListener("resize",function(){
   document.querySelector("body").appendChild(background);
 });
 
-var mouseclickX = null;
-var mouseclickY = null;
+
+let mouseclickX = null;
+let mouseclickY = null; 
 
 document.addEventListener("click",function(event){
     mouseclickX = event.pageX;
