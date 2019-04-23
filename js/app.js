1acaf621f816de5636e359bf7de15948bd55df46
ryanandrewmcpherson <ryanmcphersondev@gmail.com>
2019-04-22 20:47:04 -0400

    make links open in new tab/window

--- js/app.js
+++ js/app.js
@@ -29,11 +29,9 @@ const originalResize = evt => {
   console.log(window.innerWidth);
   if(window.matchMedia("(max-Width:596px)").matches && window.matchMedia("(min-width:497px)").matches){
     myLinksHeight = "40px";
-    console.log(myLinksHeight);
   }
   if(window.matchMedia("(max-Width:496px)").matches){
     myLinksHeight = "95.2px";
-    console.log(myLinksHeight);
   }
   if(myLinks.style.height > 0){
   myLinks.style.height = myLinksHeight;
@@ -111,39 +109,13 @@ for (let i = 0; i < coll.length; i++) {
   });
 }
 
+var linksOut = document.getElementsByTagName("a");
 
-/*
-var i;
-var lastI;
-var lastJ;
-var j;
-
-document.addEventListener("mousemove",function(event){
-  i = event.clientX;
-  j = event.clientY;
-}); */
-
-
-
-
-/* window.setInterval(function(){
-  console.log(i);
-  console.log(j);
-  x = 10;
-  var oldX = getComputedStyle(document.querySelector("body")).backgroundPositionX;
-  var oldXArr = oldX.split(",",2);
-  var y = 500*Math.sin(parseInt(oldXArr[0])/100);
-  var oldY= getComputedStyle(document.querySelector("body")).backgroundPositionY;
-  var oldYArr = oldY.split(",",2);
-
-  if(j === lastJ && i === lastI){
-    document.querySelector("body").style.backgroundPositionX = (parseInt(oldXArr[0]) + x) + "px, " + (parseInt(oldXArr[1]) - x) + "px";
-    document.querySelector("body").style.backgroundPositionY = (y-(j)) + "px, " + (y + 250 +(j)) + "px";
-
-  }else{
-  document.querySelector("body").style.backgroundPositionX = (parseInt(oldXArr[0]) + x + (i - parseInt(oldXArr[0]))) + "px, " + (parseInt(oldXArr[1]) - x - (i - parseInt(oldXArr[0]))) + "px";
-  document.querySelector("body").style.backgroundPositionY = (y-(j)) + "px, " + (y + 250 + (j)) + "px";
-  }
-  lastI = i;
-  lastJ = j
-},100) */
+for(let i=4; i < linksOut.length ; i++){
+  
+  linksOut[i].addEventListener("click",function(event){
+    event.preventDefault();
+    window.open(linksOut[i].href);
+  })
+  
+}
